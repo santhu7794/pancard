04634f80ee29333ed40bf402e16300b400c36f6c
santhu7794 <santhu.vita@gmail.com>
2024-04-15 15:30:19 +0530

    commit

--- model/panModel.js
+++ model/panModel.js
@@ -3,17 +3,20 @@ const user = new mongoose.Schema({
   Cardholder: {
     type: String,
   },
-  purpose: {
-    type: String,
-  },
-  fathername: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  mobile: {
-    type: String,
-  },
+  // Category: {
+  //   type: String,
+  // },
+  // fathername: {
+  //   type: String,
+  // },
+  // email: {
+  //   type: String,
+  // },
+  // mobile: {
+  //   type: String,
+  // },
+  image:{
+    type:String
+  }
 });
 module.exports = mongoose.model("user", user);
--- routes/panRoute.js
+++ routes/panRoute.js
@@ -2,16 +2,45 @@ const express = require("express");
 const tax = require("../model/panModel.js");
 const route = express.Router();
 const cors = require("cors");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: "photo/",
+  filename: (req, file, pan) => {
+    pan(null, file.originalname);
+  },
+});
+const photo = multer({ storage });
 
 let corsOptions = {
   orgin: ["http://localhost:4500"],
 };
-
-route.post("/addHolderDetails", cors(corsOptions), (req, res) => {
-  const pan = new tax(req.body);
-  pan.save();
-  res.status(201).json(pan);
-});
+//file upload
+route.post(
+  "/uploadphoto",
+  cors(corsOptions),
+  photo.single("file"),
+  async (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({ error: "nofile" });
+    }
+    var data = {
+      Cardholder: req.body.Cardholder,
+      image: req.file.filename,
+    };
+    try {
+      const name = await tax.create(data);
+      return res.status(200).json(name);
+    } catch (err) {
+      return res.status(500).json({ err });
+    }
+  }
+);
+// route.post("/addHolderDetails", cors(corsOptions), (req, res) => {
+//   const pan = new tax(req.body);
+//   pan.save();
+//   res.status(201).json(pan);
+// });
 route.get("/holderlist", cors(corsOptions), async (req, res) => {
   try {
     const holderlist = await tax.find();
@@ -35,8 +64,8 @@ route.delete(
   "/deleteflasepandetails/:id",
   cors(corsOptions),
   async (req, res) => {
-    const id = await tax.findByIdAndDelete(req.params.id);
-    res.status(201).json(id);
+    const deletepan = await tax.findByIdAndDelete(req.params.id);
+    res.status(201).json(deletepan);
   }
 );
 module.exports = route;
